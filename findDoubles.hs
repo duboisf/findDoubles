e9c17b1cca7cd962a4348f920f835b408a606e51
Fred Dubois <fred.dubois@gmail.com>
2012-08-05 06:31:50 -0400

    Cleaned up

--- findDoubles.hs
+++ findDoubles.hs
@@ -1,8 +1,10 @@
+import Control.Exception (catch, IOException)
 import Control.Monad (liftM, filterM, mapM, forM, forM_)
 import qualified Data.ByteString.Lazy as LS
 import Data.Digest.Pure.MD5 (md5, MD5Digest)
 import Data.List (group, sort, intercalate)
 import Foreign.C.Types (CTime)
+import Prelude hiding (catch)
 import System.Directory (getDirectoryContents, doesFileExist, doesDirectoryExist)
 import System.FilePath (splitPath, takeFileName, takeDirectory, (</>))
 import System.Environment (getArgs)
@@ -23,14 +25,15 @@ instance Ord FileInfo where
 
 getFilePaths :: FilePath -> IO [FilePath]
 getFilePaths path = putStrLn "Getting directory content" >> getFilePaths' [path]
-    where getFilePaths' []           = return []
-          getFilePaths' (path:paths) = do
-              plainFiles <- getDirectoryContents path
-              let filesWithPath = map (path </>) $ drop 2 plainFiles
-              files <- filterM doesFileExist filesWithPath
-              dirs <- filterM doesDirectoryExist filesWithPath
-              filesInSubDirs <- getFilePaths' (dirs ++ paths)
-              return (files ++ filesInSubDirs)
+  where 
+    getFilePaths' []           = return []
+    getFilePaths' (path:paths) = do
+      plainFiles <- getDirectoryContents path
+      let filesWithPath = map (path </>) $ drop 2 plainFiles
+      files <- filterM doesFileExist filesWithPath
+      dirs <- filterM doesDirectoryExist filesWithPath
+      filesInSubDirs <- getFilePaths' (dirs ++ paths)
+      return (files ++ filesInSubDirs)
 
 getHash :: FilePath -> IO FileInfo
 getHash file = do
@@ -42,33 +45,36 @@ getHash file = do
     return $ FileInfo file hash
 
 openFile' :: FilePath -> IO Handle
-openFile' file = openFile file ReadMode `catch` \_ -> do
-    performGC
-    openFile' file
+openFile' file = openFile file ReadMode `catch` reOpen
+  where
+    reOpen :: IOException -> IO Handle
+    reOpen _ = performGC >> openFile' file
 
 getHashes :: FilePath -> IO [FileInfo]
 getHashes path = do
-    paths <- getFilePaths path
-    mapM getHash paths
+  paths <- getFilePaths path
+  mapM getHash paths
 
 run :: FilePath -> FilePath -> IO ()
 run path outputFile = do
-    hashes <- getHashes path
-    let matches = map showMatches $ filter ((> 1) . length) $ group . sort $ hashes
-    writeFile outputFile $ intercalate "\n" matches
-    putStrLn "Done!"
-    where showMatches :: [FileInfo] -> String
-          showMatches aGroup = "MD5 hash: " ++ (show . fiHash . head) aGroup ++
-                               "\nFiles:\n" ++
-                               intercalate "\n" (map fiFile aGroup) ++ "\n"
+  hashes <- getHashes path
+  let matches = map showMatches $ filter ((> 1) . length) $ group . sort $ hashes
+  writeFile outputFile $ intercalate "\n" matches
+  putStrLn "Done!"
+    where
+      showMatches :: [FileInfo] -> String
+      showMatches aGroup =
+        "MD5 hash: " ++ (show . fiHash . head) aGroup ++
+        "\nFiles:\n" ++
+        intercalate "\n" (map fiFile aGroup) ++ "\n"
 
 usage :: String
 usage = "Usage: findDoubles PATH FILE\nWhere FILE is an output file for the results"
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-         (path:outputFile:[]) -> do run path outputFile
-         otherwise            -> putStrLn usage
+  args <- getArgs
+  case args of
+    (path:outputFile:[]) -> run path outputFile
+    otherwise            -> putStrLn usage
 
